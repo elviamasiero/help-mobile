66e8e872d61f0cd50a9f12f4e574a08823e8e82f
GabrielCarreiro <63800998+GabrielCarreiro@users.noreply.github.com>
2020-10-05 11:18:33 -0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { View } from 'react-native';
+import Login from './src/pages/Login'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={{paddingTop:24}}>     
+        <StatusBar style="dark" />
+        <Login/>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
