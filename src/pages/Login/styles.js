5cd5665ac13ca2b54046d3f61e4004e7b15afe7b
Elvia Goulart B. Masiero <68308804+elviamasiero@users.noreply.github.com>
2020-10-07 15:49:37 -0300

    Placeholder Email e Senha.

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,20 +2,39 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: red;
+    background-color: #ffff;
     padding: 20px;
-`;
-
-export const Input = styled.TextInput`
-    background-color: #fff;
-    margin: 10px;
-`;
-
-export const Button = styled.TouchableOpacity`
-    background-color: pink;
+    justify-content: center;
+    align-items: center; 
     
 `;
 
+export const Logo = styled.Image`
+    width: 230px;
+    height: 230px;
+    margin: 20px;
+`;
+
+export const Input = styled.TextInput`
+    background-color: black;
+    margin: 10px;
+    font-size: 19px
+    padding-vertical: 10px;
+    padding-horizontal: 180px;
+    margin-bottom: 15
+    px;
+    border-radius: 4px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    background-color: red;
+    padding: 12px;
+    align-self: center;
+    border-radius: 5px;   
+`;
+
 export const ButtonText = styled.Text`
-    color: #fff;
+    font-weight: bold;
+    font-size: 22px;    
+    color: #ffff;  
 `;
