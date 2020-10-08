c138414556a4ec7eba26495224b6a7ceeab2e821
brunoBarGon <digaprobruno@gmail.com>
2020-10-07 23:37:53 -0300

    CSS Tasks

--- src/pages/tasks/styles.js
+++ src/pages/tasks/styles.js
@@ -1,6 +1,36 @@
 import styled from 'styled-components/native';
 
-export const Icons = styled.View`
+export const Container = styled.View`
     flex: 1;
+    background-color: #ccc;
+    padding: 10px;
+    
+`;
+
+export const Tasks = styled.View`
+    flex: 1;
+`;
+
+export const TasksView = styled.View`
+    background-color: #fff;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    padding: 10px 20px;
     justify-content: space-between;
+    flex-direction: row;
+`;
+
+export const TasksText = styled.Text`
+    font-size: 16px;
+    color: #3a3a3a;
+
+`;
+
+export const TasksIcons = styled.View`
+    flex-direction: row;
+    
+`;
+
+export const TasksButtons = styled.TouchableOpacity`
+ 
 `;
